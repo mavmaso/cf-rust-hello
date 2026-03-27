4d8b96b696dc08e828ca0c8c52e2ef4054fe46a9
Marcos Soares <mavmaso@gmail.com>
2026-03-27 15:47:38 -0300

    upgrade worker sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(
-    _req: Request,
-    _env: Env,
-    _ctx: Context,
-) -> Result<Response> {
+async fn fetch(_req: Request, _env: Env, _ctx: Context,) -> Result<Response> {
     Response::ok("Hello Worker from Rust 🚀")
 }
